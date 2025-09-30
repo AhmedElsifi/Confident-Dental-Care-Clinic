da908e9d7f3e6ccf6c9a91ce5033ebae82f31af6
Ahmed Elsifi <ahmedelsifi06@gmail.com>
2025-09-30 21:01:00 +0300

    adding responsive design

--- js/main.js
+++ js/main.js
@@ -29,3 +29,33 @@ function controlledSlider(
 }
 
 controlledSlider(".dentist-slider-buttons", ".cards-list", ".card", 360, 3);
+
+const hamburger = document.getElementById("hamburger");
+const navContainer = document.getElementsByClassName("nav-container")[0];
+let linksDiv = document.createElement("div");
+linksDiv.className = "nav-details";
+linksDiv.innerHTML = `
+          <div class="nav-details-addresses">
+          <a href="#">
+            <i class="fa-solid fa-location-dot"></i>
+            <p>St. Sanguin Number 40</p>
+          </a>
+          <a href="#">
+            <i class="fa-solid fa-phone"></i>
+            <p>+1234 - 4567 - 890</p>
+          </a>
+          </div>
+          <button class="appointment-button" title="schedule a visit">
+            APPOINTMENT
+          </button>
+`;
+hamburger.addEventListener("click", (e) => {
+  navContainer.classList.toggle("active");
+  if (navContainer.contains(linksDiv)) {
+    // remove it if it’s already there
+    navContainer.removeChild(linksDiv);
+  } else {
+    // add it if it’s not
+    navContainer.appendChild(linksDiv);
+  }
+});
